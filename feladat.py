21e8fa8b3b1f07b91a6a61846fdb998fdcf3e349
HartmanMartin <hartmanmartin2@gmail.com>
2025-05-29 09:53:18 +0200

    új commit

--- feladat.py
+++ feladat.py
@@ -1,20 +1,21 @@
+#diagrammot készí
 def keszit_diagram_sort(nap_szama, homerseklet):
     csillagok_szama = int(homerseklet)
     csillagok = '*' * csillagok_szama
     sor = f"Nap {nap_szama:2}: {csillagok} ({homerseklet}°C)"
     return sor
 
-
+#rajzol egy diagrammot
 def rajzolj_diagram(homersekletek):
     print("Napi átlaghőmérséklet diagram (°C)")
     print("-" * 40)
-
+#itt egy ciklus
     for i in range(len(homersekletek)):
         nap = i + 1  # Napok számozása 1-től indul
         sor = keszit_diagram_sort(nap, homersekletek[i])
         print(sor)
 
-
+#napi hőmérsékleteket mutatja
 napi_atlaghomersekletek = [12, 15, 14, 16, 13, 17, 18]
 
 rajzolj_diagram(napi_atlaghomersekletek)
